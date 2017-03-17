589141280824719c7d6d3a1e84aaaef4f2e8806f
dulcenombre <midulce14@gmail.com>
2017-03-17 14:23:39 +0100

    1.Esta es mi primera subida del proyecto que calcula el porcentaje de alumnos que son del barcelona

---
+++ proyectodulce/src/proyectodulce/Proyectodulce.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectodulce;
+
+/**
+ *
+ * @author dulce
+ */
+public class Proyectodulce {
+
+    private static int numeroAlumnos;
+    private static int numeroAlumnosBarcelona;
+    public static double porcentaje(int numa,int numb){
+        numeroAlumnos=numa;
+        numeroAlumnosBarcelona=numb;
+        
+        return numb*100/numa;
+    }
+
+    public static void main(String[] args) {
+        
+
+        System.out.println(porcentaje(10,5)+"%");
+    }
+
+}
