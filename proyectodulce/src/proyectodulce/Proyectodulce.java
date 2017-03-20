f77a32c42a67848a5a4ce573c30e2ab440a27166
dulcenombre <midulce14@gmail.com>
2017-03-20 12:38:53 +0100

    paso final

--- proyectodulce/src/proyectodulce/Proyectodulce.java
+++ proyectodulce/src/proyectodulce/Proyectodulce.java
@@ -24,7 +24,16 @@ public class Proyectodulce {
     public static double porcentajeRM(int numa,int numRM){
         return numRM*100/numa;
     }
-
+public static void decisionFinal(double pbar,double prmd){
+    
+    if(pbar==prmd){
+        System.out.println( "¿De verdad no queremos un partido de la Champions Barça-Real Madrid?");
+    }else{
+        int x=numeroAlumnosBarcelona- numeroAlumnosRM;
+        System.out.println("Menos mal que hay" +x+ "sensatos");
+    }
+    
+}
     public static void main(String[] args) {
         numeroAlumnos=11;
         numeroAlumnosBarcelona=5;
